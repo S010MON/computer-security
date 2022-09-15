109aa80f9230f29f5e1eaf9d34983a4f35412608
Travis <travisd1711@gmail.com>
2022-09-15 16:36:46 +0200

    Java Application successfully connecting to Docker FastAPI

--- client/src/main/java/server/client/ClientApplication.java
+++ client/src/main/java/server/client/ClientApplication.java
@@ -3,11 +3,33 @@ package server.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 @SpringBootApplication
 public class ClientApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ClientApplication.class, args);
+
+		try
+		{
+			URL url = new URL("http://127.0.0.1/");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.connect();
+
+			int responseCode = conn.getResponseCode();
+			System.out.println("Response Code: " + responseCode);
+			System.out.println("Response message: " + conn.getResponseMessage());
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
+
