b14ee7ff2181d106629891678c1fa9bb7c9c60a9
Travis <travisd1711@gmail.com>
2022-09-16 16:57:53 +0200

    Auth post test currently failing

--- client/src/test/java/server/client/RequestsTest.java
+++ client/src/test/java/server/client/RequestsTest.java
@@ -21,13 +21,28 @@ public class RequestsTest
         String id = "John";
         String password = "Smith123";
         int delay = 100;
-        String steps = "[Increase1" + ", " + "Increase1]";
+        String steps = "[INCREASE 1" + ", " + "INCREASE 1]";
 
         Request request = new Request("http://127.0.0.1/auth");
         String generatedQuery = request.formatAuthRequestJSON(id, password, delay, steps);
 
         String desiredQuery = "{id: John, password: Smith123, server: {ip: 127.0.0.1, port: -1}, " +
-                "actions: {delay: 100, steps: [Increase1, Increase1]}}";
+                "actions: {delay: 100, steps: [INCREASE 1, INCREASE 1]}}";
         assertEquals(generatedQuery, desiredQuery);
+    }
+
+    @Test
+    void testAuthPostRequest()
+    {
+        String id = "John";
+        String password = "Smith123";
+        int delay = 100;
+        String steps = "[Increase1" + ", " + "Increase1]";
+
+        Request request = new Request("http://127.0.0.1/auth");
+        String generatedQuery = request.formatAuthRequestJSON(id, password, delay, steps);
+        int responseCode = request.postRequest(generatedQuery);
+        System.out.println(responseCode);
+        assertEquals(responseCode, 201);
     }
 }
