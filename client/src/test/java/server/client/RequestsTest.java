1f74647bc7f3109b8a9c478cb384f1a33290d641
Travis <travisd1711@gmail.com>
2022-09-16 16:47:37 +0200

    GetRequest successful and tested. Auth POST request format successful.

---
+++ client/src/test/java/server/client/RequestsTest.java
@@ -0,0 +1,33 @@
+package server.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//Please note the Fast API must be active for tests to successfully run
+public class RequestsTest
+{
+    @Test
+    void testGetRequest()
+    {
+        Request request = new Request("http://127.0.0.1/");
+        int response = request.getRequest();
+        assertEquals(response, 418);
+    }
+
+    @Test
+    void testAuthQueryFormat()
+    {
+        String id = "John";
+        String password = "Smith123";
+        int delay = 100;
+        String steps = "[Increase1" + ", " + "Increase1]";
+
+        Request request = new Request("http://127.0.0.1/auth");
+        String generatedQuery = request.formatAuthRequestJSON(id, password, delay, steps);
+
+        String desiredQuery = "{id: John, password: Smith123, server: {ip: 127.0.0.1, port: -1}, " +
+                "actions: {delay: 100, steps: [Increase1, Increase1]}}";
+        assertEquals(generatedQuery, desiredQuery);
+    }
+}
