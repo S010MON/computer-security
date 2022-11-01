cfc7ce6e9416542e0d90b3054200c8d01d7e832d
S010MON <leon_debnath@hotmail.co.uk>
2022-11-01 11:31:15 +0100

    added password list

--- server/app/verfiy.py
+++ server/app/verfiy.py
@@ -1,6 +1,13 @@
+common_passwords = set()
+with open("common_passwords.txt") as file:
+    data = file.readlines()
+    for d in data:
+        common_passwords.add(d)
 
 
 def valid_id(id: str) -> bool:
     return isinstance(id, str())
 
-def verify_pwd(pwd: str)  -> bool:
+
+def valid_pwd(pwd: str) -> bool:
+    return pwd not in common_passwords
